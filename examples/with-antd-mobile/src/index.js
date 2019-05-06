9c0a0d341af3005fcb9c0383f5f54f209b37d40e
lnlfps <=>
2019-05-06 17:56:26 +0800

    更新with-antd-mobile例子

--- examples/with-antd-mobile/src/index.js
+++ examples/with-antd-mobile/src/index.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react'
 import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 // import './index.less'
+import styles from './index.less'
 
 export default class Main extends React.Component {
   state = {
-    open: true,
+    open: false,
   }
   onOpenChange = (...args) => {
-    console.log(args);
     this.setState({ open: !this.state.open });
   }
   render() {
     // fix in codepen
     const sidebar = (<List>
       {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
-        if (index === 0) {
-          return (<List.Item key={index}
-                             thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-                             multipleLine
-          >Category</List.Item>);
-        }
         return (<List.Item key={index}
-                           thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                           thumb={<Icon type="check-circle-o" />}
         >Category{index}</List.Item>);
       })}
     </List>);
@@ -29,7 +23,9 @@ export default class Main extends React.Component {
     return (<div>
       <NavBar icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>Basic</NavBar>
       <Drawer
+        className={styles.myDrawer}
         enableDragHandle
+        style={{ minHeight: '100vh' }}
         contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
         sidebar={sidebar}
         open={this.state.open}
