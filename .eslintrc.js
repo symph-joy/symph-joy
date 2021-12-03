4ec9ece5081785f30a45a40d6fc8b6dc1f523d35
lnlfps <lnlfps@qq.com>
2021-12-03 10:12:02 +0800

    fix: 空格编码问题。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,29 @@
 module.exports = {
-    root: true,
-    files: ['**/*.ts?(x)'],
-    parserOptions: {
-      project: "./tsconfig.json",
-      sourceType: "module",
-      ecmaFeatures: {
-        jsx: true,
-        legacyDecorators: true,
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-      "import/internal-regex": "^next/",
-    },
-    extends: [
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-      "prettier/@typescript-eslint",
-    ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint/eslint-plugin"],
-    rules: {
-      "@typescript-eslint/no-var-requires": 0,
-      "@typescript-eslint/ban-types": 0,
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "@typescript-eslint/no-empty-function": "warn",
-      "prefer-const": "off",
-      "max-len": [0, { code: 150 }]
-    },
-  };
-
+  root: true,
+  files: ["**/*.ts?(x)"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      legacyDecorators: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/internal-regex": "^next/",
+  },
+  extends: ["plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/ban-types": 0,
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-empty-function": "warn",
+    "prefer-const": "off",
+    "max-len": [0, { code: 150 }],
+  },
+};
