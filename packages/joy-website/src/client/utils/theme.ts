f18c220b6b3051d2448e0fe82ef67ca02c39bdce
shenchao <962965494@qq.com>
2021-12-29 14:52:45 +0800

    fix: 解决页面样式跳动的bug

--- packages/joy-website/src/client/utils/theme.ts
+++ packages/joy-website/src/client/utils/theme.ts
@@ -5,12 +5,12 @@ export function getTheme() {
 }
 
 export function changeTheme() {
-  const newTheme = getTheme() === 'dark' ? 'light' : 'dark';
+  const newTheme = getTheme() === "dark" ? "light" : "dark";
   const themeUrl = newTheme === "dark" ? "/static/antd.dark.css" : "/static/antd.css";
   const oBody = document.getElementsByTagName("body")[0];
   const oLink = document.getElementById("theme-style") as HTMLLinkElement;
 
+  localStorage.setItem("theme", newTheme);
   oLink.href = themeUrl;
-
   oBody.setAttribute("data-theme", newTheme);
 }
