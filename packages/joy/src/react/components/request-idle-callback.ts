676b2e894960021faad026ea0235e2377a325487
lnlfps <lnlfps@qq.com>
2022-03-04 14:55:03 +0800

    feat: 添加图片组件和图片优化服务。

--- packages/joy/src/react/components/request-idle-callback.ts
+++ packages/joy/src/react/components/request-idle-callback.ts
@@ -1,23 +1,19 @@
 export const requestIdleCallback =
-  (typeof self !== 'undefined' &&
-    self.requestIdleCallback &&
-    self.requestIdleCallback.bind(window)) ||
+  (typeof self !== "undefined" && self.requestIdleCallback && self.requestIdleCallback.bind(window)) ||
   function (cb: IdleRequestCallback): number {
-    let start = Date.now()
+    let start = Date.now();
     return setTimeout(function () {
       cb({
         didTimeout: false,
         timeRemaining: function () {
-          return Math.max(0, 50 - (Date.now() - start))
+          return Math.max(0, 50 - (Date.now() - start));
         },
-      })
-    }, 1) as unknown as number
-  }
+      });
+    }, 1) as unknown as number;
+  };
 
 export const cancelIdleCallback =
-  (typeof self !== 'undefined' &&
-    self.cancelIdleCallback &&
-    self.cancelIdleCallback.bind(window)) ||
+  (typeof self !== "undefined" && self.cancelIdleCallback && self.cancelIdleCallback.bind(window)) ||
   function (id: number) {
-    return clearTimeout(id)
-  }
+    return clearTimeout(id);
+  };
